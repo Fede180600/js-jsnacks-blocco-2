4f3809ca1f3a6012de0d637b8b719484f59739f5
Fede180600 <fede180600@libero.it>
2022-04-20 15:47:54 +0200

    jsnack3 js pseudo-code

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,2 +1,7 @@
 // jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
+// Chiedo all'utente di inserire un numero
+
+// Parte calcoli 
+
+// Stampo i risultati in console
