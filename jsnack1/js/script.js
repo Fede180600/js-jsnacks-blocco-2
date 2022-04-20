f4d49fb2e118f08b016ba1246d54f2472ef00038
Fede180600 <fede180600@libero.it>
2022-04-20 14:49:03 +0200

    jsnack1 for cycle

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,3 +1,18 @@
 // jsnack1. Il software deve chiedere per 5 volte all'utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+
+// Metodo con ciclo for
+
+// Ciclo (for) per chiedere 5 volte di inserire un numero
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+    const userNumber = parseInt(prompt("Inserisci un numero"));
+    sum += userNumber
+}
+
+// Stampo in console la somma dei 5 numeri inseriti 
+console.log(sum);
+
