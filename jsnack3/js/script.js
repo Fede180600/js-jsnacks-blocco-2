2f4457f3012506d2bd1cab790b00de510af430d9
Fede180600 <fede180600@libero.it>
2022-04-20 16:27:02 +0200

    jsnack3 finish

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,7 +1,15 @@
 // jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
-
+const myNumber = [];
 // Chiedo all'utente di inserire un numero
-
+const userNumber = prompt("Dimmi un numero");
+console.log(userNumber);
+myNumber.push(userNumber);
+console.log("Array col numero inserito dall'utente:", myNumber);
 // Parte calcoli 
-
-// Stampo i risultati in console
+for (let i = 0; i < userNumber.length; i++) {
+    const numberToElevate = parseInt(userNumber[i]);
+    const numberElevate = Math.pow(numberToElevate, 3);
+    
+    // Stampo i risultati in console
+    console.log(`Cubo di ${numberToElevate} =`, numberElevate);
+}
