99ba9322668a10160c653d755892c1da5fc130c9
Fede180600 <fede180600@libero.it>
2022-04-20 14:38:11 +0200

    scaffolding jsnack1

---
+++ jsnack1/js/script.js
@@ -0,0 +1,3 @@
+// jsnack1. Il software deve chiedere per 5 volte all'utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
