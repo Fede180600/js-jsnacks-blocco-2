fb91a61d4bfde58ae0998a841bde4f633ee51f86
Fede180600 <fede180600@libero.it>
2022-04-20 15:09:24 +0200

    add jsnack2 names & surnames arrays

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,2 +1,6 @@
 // jsnack2. Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e una
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+// Creo le array dei nomi e cognomi 
+const gusetsNames = ["Alberto", "Giovanni", "Edoardo", "Nicola", "Francesca", "Alessia", "Simone", "Nicole", "Sara"];
+const guestsSurnames = ["Malvezzi", "Rossi", "Bergamini", "Farci", "Setti", "Malvezzi", "Valeri", "Corradi"];
