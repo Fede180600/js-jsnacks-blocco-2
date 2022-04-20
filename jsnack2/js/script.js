a31b1632d2061a1279cdf29cd96cb950baac1934
Fede180600 <fede180600@libero.it>
2022-04-20 15:18:21 +0200

    add random name calc

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -2,5 +2,11 @@
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
 // Creo le array dei nomi e cognomi 
-const gusetsNames = ["Alberto", "Giovanni", "Edoardo", "Nicola", "Francesca", "Alessia", "Simone", "Nicole", "Sara"];
+const guestsNames = ["Alberto", "Giovanni", "Edoardo", "Nicola", "Francesca", "Alessia", "Simone", "Nicole", "Sara"];
 const guestsSurnames = ["Malvezzi", "Rossi", "Bergamini", "Farci", "Setti", "Malvezzi", "Valeri", "Corradi"];
+
+// Genero un nome e un cognome casuali dalle arrays
+const randomNameIndex = Math.floor(Math.random() * guestsNames.length);
+const randomName = guestsNames [randomNameIndex];
+console.log("Posizione nella array dei nomi:", randomNameIndex);
+console.log("Nome corrispondente:", randomName);
